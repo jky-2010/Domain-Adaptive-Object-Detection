04c6a8559376f4e50639314e5d7d831516c75659
Elias <eliasmapendo97@gmail.com>
2025-05-05 23:23:51 -0700

    updated utils.py

--- data/utils.py
+++ data/utils.py
@@ -2,7 +2,7 @@
 Author: Elias Mapendo
 Date: May 03, 2025
 Description:
-data/utils.py utility for collate fn.
+data/utils.py utility for collate function and dataloader construction.
 """
 
 from torch.utils.data import DataLoader, Subset
@@ -11,27 +11,39 @@ from data.datasets import CityscapesDataset
 from data.preprocessing import BasicTransform
 
 def collate_fn(batch):
+    # Custom collate function for object detection datasets
     return tuple(zip(*batch))
 
-def get_dataloaders(self):
-    """Prepare DataLoaders for source (clear) and target (foggy) domains."""
+def get_dataloaders(batch_size, target_labels, num_workers=2):
+    """
+    Prepare DataLoaders for source (clear) and target (foggy) domains.
+
+    Args:
+        batch_size (int): Number of samples per batch.
+        target_labels (list): List of label IDs to include.
+        num_workers (int): Number of subprocesses for data loading.
+
+    Returns:
+        Tuple[DataLoader, DataLoader]: source_loader, target_loader
+    """
     transform = BasicTransform()
 
-    # SOURCE (clear)
+    # Clear weather (source domain)
     source_dataset = CityscapesDataset(mode='train', foggy=False, transforms=transform,
-                                       target_labels=self.target_labels)
+                                       target_labels=target_labels)
     subset_size_src = int(0.8 * len(source_dataset))
     source_subset = Subset(source_dataset, random.sample(range(len(source_dataset)), subset_size_src))
 
-    # TARGET (foggy)
+    # Foggy weather (target domain)
     target_dataset = CityscapesDataset(mode='train', foggy=True, transforms=transform,
-                                       target_labels=self.target_labels)
+                                       target_labels=target_labels)
     subset_size_tgt = int(0.8 * len(target_dataset))
     target_subset = Subset(target_dataset, random.sample(range(len(target_dataset)), subset_size_tgt))
 
-    source_loader = DataLoader(source_subset, batch_size=self.batch_size, shuffle=True,
-                               collate_fn=collate_fn, num_workers=2)
-    target_loader = DataLoader(target_subset, batch_size=self.batch_size, shuffle=True,
-                               collate_fn=collate_fn, num_workers=2)
+    # Create DataLoaders
+    source_loader = DataLoader(source_subset, batch_size=batch_size, shuffle=True,
+                               collate_fn=collate_fn, num_workers=num_workers)
+    target_loader = DataLoader(target_subset, batch_size=batch_size, shuffle=True,
+                               collate_fn=collate_fn, num_workers=num_workers)
 
     return source_loader, target_loader
