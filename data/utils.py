195b559fcac1f6780f2f338f71e521a4f95812db
Elias <eliasmapendo97@gmail.com>
2025-05-05 23:15:26 -0700

    move get_dataloaders() to data/utils.p

--- data/utils.py
+++ data/utils.py
@@ -5,6 +5,33 @@ Description:
 data/utils.py utility for collate fn.
 """
 
+from torch.utils.data import DataLoader, Subset
+import random
+from data.datasets import CityscapesDataset
+from data.preprocessing import BasicTransform
 
 def collate_fn(batch):
     return tuple(zip(*batch))
+
+def get_dataloaders(self):
+    """Prepare DataLoaders for source (clear) and target (foggy) domains."""
+    transform = BasicTransform()
+
+    # SOURCE (clear)
+    source_dataset = CityscapesDataset(mode='train', foggy=False, transforms=transform,
+                                       target_labels=self.target_labels)
+    subset_size_src = int(0.8 * len(source_dataset))
+    source_subset = Subset(source_dataset, random.sample(range(len(source_dataset)), subset_size_src))
+
+    # TARGET (foggy)
+    target_dataset = CityscapesDataset(mode='train', foggy=True, transforms=transform,
+                                       target_labels=self.target_labels)
+    subset_size_tgt = int(0.8 * len(target_dataset))
+    target_subset = Subset(target_dataset, random.sample(range(len(target_dataset)), subset_size_tgt))
+
+    source_loader = DataLoader(source_subset, batch_size=self.batch_size, shuffle=True,
+                               collate_fn=collate_fn, num_workers=2)
+    target_loader = DataLoader(target_subset, batch_size=self.batch_size, shuffle=True,
+                               collate_fn=collate_fn, num_workers=2)
+
+    return source_loader, target_loader
